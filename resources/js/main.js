26898ecb7c1af471f6f60c32a650538c274a5e99
Chip Long <chip_long@yahoo.com>
2019-11-02 20:48:21 -0600

    NavBar added

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,9 @@
-$(document).ready( function () {
-  
-});
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function topNavFunc() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
