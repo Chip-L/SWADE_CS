4ddc9a07f91fa2e752c2125339c466b4f2845316
Chip Long <chip_long@yahoo.com>
2019-11-03 13:27:12 -0700

    Fixed TopNav highlights

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,5 +1,31 @@
+//import html_utils from './html_utils';
+
+const eMenuHinderances = document.getElementById('menu-hinderances');
+const eMenuTraits = document.getElementById('menu-traits');
+const eMenuEdges = document.getElementById('menu-edges');
+const eMenuPowers = document.getElementById('menu-powers');
+
+function menuClick(item) {
+  eMenuHinderances.classList.remove('active');
+  eMenuTraits.classList.remove('active');
+  eMenuEdges.classList.remove('active');
+  eMenuPowers.classList.remove('active');
+
+  if (item==='hinderances') {
+    eMenuHinderances.classList.add('active');
+  } else if (item==='traits') {
+    eMenuTraits.classList.add('active');
+  } else if (item==='edges') {
+    eMenuEdges.classList.add('active');
+  } else if (item==='powers') {
+    eMenuPowers.classList.add('active');
+  } else {
+    console.log('menuClick not understood');
+  }
+}
+
 /* Toggle between adding and removing the "responsive" class to topnav when the
- user clicks on the icon */
+user clicks on the icon */
 function topNavFunc() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -8,3 +34,6 @@ function topNavFunc() {
     x.className = "topnav";
   }
 }
+
+
+//menuHinderances.addEventListener("click", menuClick('hinderances'));
